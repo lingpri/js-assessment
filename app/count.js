72b8d6b200047162ac1ae018b323a58297e24719
Priya Rammohan <varpri@gmail.com>
2016-08-31 09:12:01 -0500

    Initial Version

--- app/count.js
+++ app/count.js
@@ -3,5 +3,27 @@ exports = typeof window === 'undefined' ? global : window;
 exports.countAnswers = {
   count: function (start, end) {
 
+  	var td;
+    function print() {
+      console.log(start); 
+      
+       start = start + 1;
+
+      if (start <= end) {
+        td = setTimeout(print, 100);
+
+      }
+    }
+
+    print();
+
+    return {
+      cancel: function () {
+      	if(td)
+          clearTimeout(td);
+      	
+      }
+    };
+
   }
 };
