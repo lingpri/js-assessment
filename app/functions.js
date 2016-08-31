0ed2d27491774474d89a3bfcdc3cd13601773cc7
Priya Rammohan <varpri@gmail.com>
2016-08-31 12:00:42 -0500

    Initial Version

--- app/functions.js
+++ app/functions.js
@@ -3,13 +3,21 @@ exports = typeof window === 'undefined' ? global : window;
 exports.functionsAnswers = {
   argsAsArray: function(fn, arr) {
 
+     return fn.apply(null, arr);
+
   },
 
   speak: function(fn, obj) {
 
+    return fn.call(obj);
+
   },
 
   functionFunction: function(str) {
+
+    return function(arg) {
+      return str + ', ' + arg;
+    };
 
   },
 
@@ -19,18 +27,35 @@ exports.functionsAnswers = {
 
   partial: function(fn, str1, str2) {
 
+    return function(str3) {
+      return fn(str1, str2, str3);
+    };
+
   },
 
   useArguments: function() {
+
+
+    //convert arguments to Array
+    var args = Array.prototype.slice.call(arguments);
+
+    //use the reduce function to compute total
+    var total = args.reduce(function(a, b) {
+            return a + b;
+        }, 0);
+
+        return total;
 
   },
 
   callIt: function(fn) {
 
+    
   },
 
   partialUsingArguments: function(fn) {
 
+   
   },
 
   curryIt: function(fn) {
--- app/strings.js
+++ app/strings.js
@@ -3,6 +3,8 @@ exports = typeof window === 'undefined' ? global : window;
 exports.stringsAnswers = {
   reduceString: function(str, amount) {
 
+  	
+
   },
 
   wordWrap: function(str, cols) {
